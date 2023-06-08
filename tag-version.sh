afb749793222bfe863446d1363ef6cab42e63aa5
Alessandro Mariotti <alessandro.mariotti@zupit.it>
2023-06-08 16:22:27 +0200

    fix: fix version tagging

--- tag-version.sh
+++ tag-version.sh
@@ -1,10 +1,6 @@
 export OLDV=$1
 export NEWV=$2
 echo "Tagging workflows from v${OLDV} to v${NEWV}"
-ls -lah
-pwd
-ls -lah .github
 find .github -name "*.yml" -exec sed -E -i 's/\@v'$OLDV'/\@v'$NEWV'/g' {} \;
-git status
-git diff
-
+git commit -am "chore(release): tag all workflows with new version $NEWV"
+git push
