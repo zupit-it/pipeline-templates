224d15fac0a506795b2686ee1bc04c9f3a813111
Alessandro Mariotti <alessandro.mariotti@zupit.it>
2023-06-09 08:50:02 +0200

    fix: sync versions in README, add README to updated files on release

--- tag-version.sh
+++ tag-version.sh
@@ -2,3 +2,4 @@ export OLDV=$1
 export NEWV=$2
 echo "Tagging workflows from v${OLDV} to v${NEWV}"
 find .github -name "*.yml" -exec sed -E -i 's/\@v'$OLDV'/\@v'$NEWV'/g' {} \;
+sed -E -i 's/\@v'$OLDV'/\@v'$NEWV'/g' README.md
