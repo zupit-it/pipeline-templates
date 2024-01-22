c502f18385a16e305c2bcfb522117689730ddc4e
Alessandro Mariotti <alessandro.mariotti@zupit.it>
2024-01-22 10:54:22 +0100

    fix: improve build method and docker image

--- docker/sonar-scanner-cli/build.sh
+++ docker/sonar-scanner-cli/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker buildx build -t ghcr.io/zupit-it/pipeline-templates/sonar-scanner-cli:5.0.1 -f Dockerfile .
+docker buildx build -t ghcr.io/zupit-it/pipeline-templates/sonar-scanner-cli:5.0.1 -t ghcr.io/zupit-it/pipeline-templates/sonar-scanner-cli:latest -f Dockerfile .
+docker push ghcr.io/zupit-it/pipeline-templates/sonar-scanner-cli:5.0.1
+docker push ghcr.io/zupit-it/pipeline-templates/sonar-scanner-cli:latest
