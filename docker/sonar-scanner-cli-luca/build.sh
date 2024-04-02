4881e5116abeb28509499811baa777e3d338a616
Davide Bontempelli <114908971+davide-bontempelli-zupit@users.noreply.github.com>
2024-04-02 11:32:28 +0200

    feat(ZCH-66): cleanup sonar scan and quality gate steps (#84)
    * feat(ZCH-66): pass working dir
    
    * fix: fix basedir
    
    * fix: update step
    
    * chore: simplify pipeline
    
    * chore: change default
    
    * chore: remove deprecated code
    
    * chore: add var for quality gate
    
    * chore: restore old sonar flutter
    
    * chore: remove old vars
    
    * chore: rename to flutter

--- docker/sonar-scanner-cli-luca/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker buildx build -t ghcr.io/zupit-it/pipeline-templates/sonar-scanner-cli-luca:5.0.1 -t ghcr.io/zupit-it/pipeline-templates/sonar-scanner-cli-luca:latest -f Dockerfile .
-docker push ghcr.io/zupit-it/pipeline-templates/sonar-scanner-cli-luca:5.0.1
-docker push ghcr.io/zupit-it/pipeline-templates/sonar-scanner-cli-luca:latest
