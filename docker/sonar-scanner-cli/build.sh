0b864f1a1b2a2354f8672769211edcaedd149d88
Davide Bontempelli <davide.bontempelli@zupit.it>
2024-04-02 08:13:34 +0200

    chore: rename to flutter

--- docker/sonar-scanner-cli/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker buildx build -t ghcr.io/zupit-it/pipeline-templates/sonar-scanner-cli:5.0.1 -t ghcr.io/zupit-it/pipeline-templates/sonar-scanner-cli:latest -f Dockerfile .
-docker push ghcr.io/zupit-it/pipeline-templates/sonar-scanner-cli:5.0.1
-docker push ghcr.io/zupit-it/pipeline-templates/sonar-scanner-cli:latest
