a583822b9c29b631a3f798b4a4df79e2b804c3aa
Alessandro Mariotti <alessandro.mariotti@zupit.it>
2024-04-24 16:18:55 +0200

    chore: upgrade flutter version

--- docker/flutter-sonar-scanner-cli/build.sh
+++ docker/flutter-sonar-scanner-cli/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker buildx build -t ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:5.0.1 -t ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:latest -f Dockerfile .
-docker push ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:5.0.1
-docker push ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:latest
+docker buildx build -t ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:sonarqube5.0.1-flutter3.19.3 -f Dockerfile .
+docker push ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:sonarqube5.0.1-flutter3.19.3
+# docker push ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:latest
