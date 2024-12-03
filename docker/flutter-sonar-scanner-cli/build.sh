6b3034ecb88a56e0fec44f328077e8b8a9d90243
Alessio Libardi <115072949+alessio-libardi-zupit@users.noreply.github.com>
2024-12-03 13:26:56 +0100

    chore: update flutter version

--- docker/flutter-sonar-scanner-cli/build.sh
+++ docker/flutter-sonar-scanner-cli/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker buildx build -t ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:sonarqube5.0.1-flutter3.19.3 -f Dockerfile .
-docker push ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:sonarqube5.0.1-flutter3.19.3
+docker buildx build -t ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:sonarqube5.0.1-flutter3.22.3 -f Dockerfile .
+docker push ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:sonarqube5.0.1-flutter3.22.3
 # docker push ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:latest
