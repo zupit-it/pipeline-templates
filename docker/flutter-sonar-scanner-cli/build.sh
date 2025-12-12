bee0df3c5ef48299bfeb8d545ce9b6614e9cb0f5
Davide Bontempelli <davide.bontempelli@zupit.it>
2025-12-12 12:15:34 +0100

    feat: update Flutter sonar scanner to 3.38.4

--- docker/flutter-sonar-scanner-cli/build.sh
+++ docker/flutter-sonar-scanner-cli/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker buildx build -t ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:sonarqube5.0.1-flutter3.22.3 -f Dockerfile .
-docker push ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:sonarqube5.0.1-flutter3.22.3
+docker buildx build -t ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:sonarqube5.0.1-flutter3.38.4 -f Dockerfile .
+docker push ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:sonarqube5.0.1-flutter3.38.4
 # docker push ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:latest
