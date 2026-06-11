4b863879bd1c8a4abbf70b6e4c28ec603f84450d
Davide Bontempelli <114908971+davide-bontempelli-zupit@users.noreply.github.com>
2026-06-11 12:28:05 +0200

    feat(TSTR-1): update GitHub actions for Node 24 runtime (#153)

--- tag-version.sh
+++ tag-version.sh
@@ -2,4 +2,5 @@ export OLDV=$1
 export NEWV=$2
 echo "Tagging workflows from v${OLDV} to v${NEWV}"
 find .github -name "*.yml" -exec sed -E -i 's/\@v'$OLDV'/\@v'$NEWV'/g' {} \;
+find .github -name "*.yml" -exec sed -E -i 's/\@TSTR-1\/update-node24-actions/\@v'$NEWV'/g' {} \;
 sed -E -i 's/\@v'$OLDV'/\@v'$NEWV'/g' README.md
