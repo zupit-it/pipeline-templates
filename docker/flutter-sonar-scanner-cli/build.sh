70267c58186b12be8879fded771f2db69f824441
Marco Bacis <marco.bacis@zupit.it>
2026-07-10 12:03:25 +0200

    chore: update flutter sonar image to 3.41.9

--- docker/flutter-sonar-scanner-cli/build.sh
+++ docker/flutter-sonar-scanner-cli/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker buildx build -t ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:sonarqube5.0.1-flutter3.38.4 -f Dockerfile .
-docker push ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:sonarqube5.0.1-flutter3.38.4
+docker buildx build -t ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:sonarqube5.0.1-flutter3.41.9 -f Dockerfile .
+docker push ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:sonarqube5.0.1-flutter3.41.9
 # docker push ghcr.io/zupit-it/pipeline-templates/flutter-sonar-scanner-cli:latest
